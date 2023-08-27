f233bd1267278410dd539f444305abdba4b0e52f
Lokesh <iamlokeshahire@gmail.com>
2023-08-28 00:29:07 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const dbConnection = mongoose.connect(process.env.mongoUrl);
+const dbConnection = mongoose.connect(
+  "mongodb+srv://lokesh:ahire@cluster0.entjnlc.mongodb.net/Levitation"
+);
 
 module.exports = {
   dbConnection,
